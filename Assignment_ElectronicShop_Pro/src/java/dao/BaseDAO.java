fb7e7b11e63834c270f07e768e7a35c3195b9799
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-06-19 21:09:11 +0700

    Fix Fault 2: Delete unused import in BaseDAO

--- Assignment_ElectronicShop_Pro/src/java/dao/BaseDAO.java
+++ Assignment_ElectronicShop_Pro/src/java/dao/BaseDAO.java
@@ -8,7 +8,6 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
